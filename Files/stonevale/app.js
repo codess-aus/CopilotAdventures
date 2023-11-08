eb7a03335e1ca2e68662870e3ee1ef8db6a364dc
Michelle Mei-Ling Sandford <codesswa@outlook.com>
2023-11-08 18:38:06 +0800

    Update app.js

--- Files/stonevale/app.js
+++ Files/stonevale/app.js
@@ -1,1 +1,12 @@
-
+// Simulate the duel between two warriors Rok and Papyra. 
+// Each warrior makes a series of moves, and each move has a specific outcome. 
+// To win the duel, a warrior must accumulate the highest score over a series of rounds.
+// Each warrior can make one of three moves: rock, paper, or scissors.
+// A rock win = 1 point
+// A paper win = 2 points
+// A scissors win = 3 points
+// When both warriors choose the same move, it's a draw, and no points are awarded.
+// Rock crushes scissors, paper covers rock, and scissors cut paper.
+// Points are awarded based on the winning move.
+// The duel consists of 5 rounds.
+// The goal is to accumulate the highest score over these rounds to win the duel.
